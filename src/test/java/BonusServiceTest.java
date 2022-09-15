74a19b04055fa0357290cd2f4afc20512c390225
Anastasia <Beanasta@mail.ru>
2022-09-15 14:37:47 +0300

    Reformat code

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -3,147 +3,147 @@ import org.junit.jupiter.api.Test;
 
 public class BonusServiceTest {
 
-        @Test
-        void shouldCalculateForRegisteredAndUnderLimit() {
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 1_000;
-            boolean registered = true;
-            long expected = 30;
+        long amount = 1_000;
+        boolean registered = true;
+        long expected = 30;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
-        @Test
-        void shouldCalculateForRegisteredAndOverLimit() {
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 1_000_000;
-            boolean registered = true;
-            long expected = 500;
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
-        @Test
-        void shouldCalculateForUnRegisteredAndOverLimit() {
+    @Test
+    void shouldCalculateForUnRegisteredAndOverLimit() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 50_100;
-            boolean registered = false;
-            long expected = 500;
+        long amount = 50_100;
+        boolean registered = false;
+        long expected = 500;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
-        }
+    }
 
-        @Test
-        void shouldCalculateForUnRegisteredAndUnderLimit() {
+    @Test
+    void shouldCalculateForUnRegisteredAndUnderLimit() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 49_999;
-            boolean registered = false;
-            long expected = 499;
+        long amount = 49_999;
+        boolean registered = false;
+        long expected = 499;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
-        }
+    }
 
-        @Test
-        void shouldCalculateForUnRegisteredAndZeroAmount() {
+    @Test
+    void shouldCalculateForUnRegisteredAndZeroAmount() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 0;
-            boolean registered = false;
-            long expected = 0;
+        long amount = 0;
+        boolean registered = false;
+        long expected = 0;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
-        @Test
-        void shouldCalculateForRegisteredAndZeroAmount() {
+    @Test
+    void shouldCalculateForRegisteredAndZeroAmount() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 0;
-            boolean registered = true;
-            long expected = 0;
+        long amount = 0;
+        boolean registered = true;
+        long expected = 0;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
-        @Test
-        void shouldCalculateForUnRegisteredAndZeroBonus() {
+    @Test
+    void shouldCalculateForUnRegisteredAndZeroBonus() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 99;
-            boolean registered = false;
-            long expected = 0;
+        long amount = 99;
+        boolean registered = false;
+        long expected = 0;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
 
-        }
+    }
 
-        @Test
-        void shouldCalculateForRegisteredAndZeroBonus() {
+    @Test
+    void shouldCalculateForRegisteredAndZeroBonus() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 33;
-            boolean registered = true;
-            long expected = 0;
+        long amount = 33;
+        boolean registered = true;
+        long expected = 0;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
-        @Test
-        void shouldCalculateForUnRegisteredAndMinBonus() {
+    @Test
+    void shouldCalculateForUnRegisteredAndMinBonus() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 100;
-            boolean registered = false;
-            long expected = 1;
+        long amount = 100;
+        boolean registered = false;
+        long expected = 1;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
-        @Test
-        void shouldCalculateForRegisteredAndMinBonus() {
+    @Test
+    void shouldCalculateForRegisteredAndMinBonus() {
 
-            BonusService service = new BonusService();
+        BonusService service = new BonusService();
 
-            long amount = 34;
-            boolean registered = true;
-            long expected = 1;
+        long amount = 34;
+        boolean registered = true;
+        long expected = 1;
 
-            long actual = service.calculate(amount, registered);
+        long actual = service.calculate(amount, registered);
 
-            Assertions.assertEquals(expected, actual);
-        }
+        Assertions.assertEquals(expected, actual);
+    }
 
 }
