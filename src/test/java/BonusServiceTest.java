96ddc2c05875f5fdb82e300adcb85b2654c6e0c1
Anastasia <Beanasta@mail.ru>
2022-09-15 13:33:31 +0300

    Initial commit

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,149 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BonusServiceTest {
+
+        @Test
+        void shouldCalculateForRegisteredAndUnderLimit() {
+
+            BonusService service = new BonusService();
+
+            long amount = 1_000;
+            boolean registered = true;
+            long expected = 30;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldCalculateForRegisteredAndOverLimit() {
+
+            BonusService service = new BonusService();
+
+            long amount = 1_000_000;
+            boolean registered = true;
+            long expected = 500;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldCalculateForUnRegisteredAndOverLimit() {
+
+            BonusService service = new BonusService();
+
+            long amount = 50_100;
+            boolean registered = false;
+            long expected = 500;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+
+        }
+
+        @Test
+        void shouldCalculateForUnRegisteredAndUnderLimit() {
+
+            BonusService service = new BonusService();
+
+            long amount = 49_999;
+            boolean registered = false;
+            long expected = 499;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+
+        }
+
+        @Test
+        void shouldCalculateForUnRegisteredAndZeroAmount() {
+
+            BonusService service = new BonusService();
+
+            long amount = 0;
+            boolean registered = false;
+            long expected = 0;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldCalculateForRegisteredAndZeroAmount() {
+
+            BonusService service = new BonusService();
+
+            long amount = 0;
+            boolean registered = true;
+            long expected = 0;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldCalculateForUnRegisteredAndZeroBonus() {
+
+            BonusService service = new BonusService();
+
+            long amount = 99;
+            boolean registered = false;
+            long expected = 0;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+
+        }
+
+        @Test
+        void shouldCalculateForRegisteredAndZeroBonus() {
+
+            BonusService service = new BonusService();
+
+            long amount = 33;
+            boolean registered = true;
+            long expected = 0;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldCalculateForUnRegisteredAndMinBonus() {
+
+            BonusService service = new BonusService();
+
+            long amount = 100;
+            boolean registered = false;
+            long expected = 1;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldCalculateForRegisteredAndMinBonus() {
+
+            BonusService service = new BonusService();
+
+            long amount = 34;
+            boolean registered = true;
+            long expected = 1;
+
+            long actual = service.calculate(amount, registered);
+
+            Assertions.assertEquals(expected, actual);
+        }
+
+}
